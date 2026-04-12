563a7f173e10f0ebfd21d34aff2ad99a61e8b95f
samarth <samarthsoni3002@gmail.com>
2026-04-12 15:20:53 +0530

    feat: add skip-gram and cbow model classes

---
+++ word2vec/models.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+
+
+# Skip-gram Model
+
+class SkipgramModel(nn.Module):
+
+  def __init__(self,vocab_size,embedding_dim):
+    super().__init__()
+    self.in_embedding = nn.Embedding(num_embeddings=vocab_size,embedding_dim=embedding_dim)
+    self.out_embedding = nn.Embedding(num_embeddings=vocab_size,embedding_dim=embedding_dim)
+
+  def forward(self,center_ids):
+
+    center_emb = self.in_embedding(center_ids)
+    logits = center_emb @ self.out_embedding.weight.T
+
+    return logits 
+
+
+# Cbow Model 
+
+class CbowModel(nn.Module):
+
+  def __init__(self,vocab_size,embedding_dim,pad_id):
+    super().__init__()
+    self.pad_id = pad_id
+    self.in_embedding = nn.Embedding(num_embeddings=vocab_size+1,embedding_dim=embedding_dim,padding_idx=pad_id)
+    self.out_embedding = nn.Embedding(num_embeddings=vocab_size,embedding_dim=embedding_dim)
+
+  def forward(self,context_ids):
+
+    context_emb = self.in_embedding(context_ids)
+
+    mask = (context_ids != self.pad_id).unsqueeze(-1)
+    masked_emb = context_emb * mask
+
+    summed = masked_emb.sum(dim=1)
+    counts = mask.sum(dim=1).clamp(min=1)
+
+    avg_emb = summed / counts
+
+    logits = avg_emb @ self.out_embedding.weight.T
+
+    return logits
